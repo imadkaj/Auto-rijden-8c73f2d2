014d48fd18757ed5f75f91bd8a96d6f91082b860
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-20 14:19:50 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,10 @@
+<?php 
+
+$a = readline("hoe oud ben je?");
+ 
+if ($a>=16.5) {
+    print("je mag beginnen met rijlessen!");
+}
+else{
+    print("helaas, je mag nog niet beginnen met rijlessen");
+}
